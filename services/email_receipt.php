d80f5420bd9566459090690570fa5666b1c19dc0
Jim Hooper <jim@massrelevance.com>
2015-06-27 11:54:07 -0500

    Removed Size from receipt

--- services/email_receipt.php
+++ services/email_receipt.php
@@ -67,12 +67,16 @@
            
           $i = 0;
           for($i;$i<$items_count;$i++){
-            $rmsg .= "<b>Product:</b> " . $items[$i]['itemName']
-              . "<br /><b>Description:</b> " . $items[$i]['description']
-              . "<br /><b>Amount:</b> " . $items[$i]['amount'];
+            if(isset($items[$i]['clothe'])){
+                $rmsg .= "<b>Product:</b> Pet Portrait";
+             }else{
+               $rmsg .= "<b>Product:</b> " . $items[$i]['itemName']
+              . "<br /><b>Description:</b> " . $items[$i]['description'];
+            }
+              $rmsg .= "<br /><b>Amount:</b> " . $items[$i]['amount'];
             
             if(isset($items[$i]['size'])){
-              $rmsg .= "<br /><b>Size:</b> " . $items[$i]['size'];
+             // $rmsg .= "<br /><b>Size:</b> " . $items[$i]['size'];
             }
             if(isset($items[$i]['clothe'])){
               $pets_query = $DB->query("SELECT * FROM order_pet LEFT JOIN clothes ON clothes.clothes_id=order_pet.order_pet_clothe_ref_id WHERE order_pet_session_id='$stored_session'");
